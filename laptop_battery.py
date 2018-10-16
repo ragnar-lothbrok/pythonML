8fbf0d16ad36cab0beef91537cbdc6c8203652e9
RedViper <raghunitb@gmail.com>
2018-10-16 14:13:05 +0800

    Update laptop_battery.py

--- laptop_battery.py
+++ laptop_battery.py
@@ -1,25 +1,23 @@
 #!/bin/python3
-
-import math
-import os
-import random
-import re
-import sys
-
 import pandas as pd
 from sklearn import linear_model
-import sys
+from sklearn.model_selection import train_test_split
 
 def getPred(timeCharged):
     trainCsvData = pd.read_csv("https://s3.amazonaws.com/hr-testcases/399/assets/trainingdata.txt", sep=",",
                                names=['time_charged', 'battery_lasted'])
+
     trainCsvData['time_charged'] = trainCsvData['time_charged']
     trainCsvData['battery_lasted'] = trainCsvData['battery_lasted']
+
+    trainCsvData = trainCsvData.sample(frac=1)
 
     trainCsvData['battery_lasted'] = trainCsvData['battery_lasted'].astype(int)
 
     trainX = trainCsvData.as_matrix(['time_charged'])
     trainY = trainCsvData['battery_lasted']
+
+    trainX, testX, trainY, y_test = train_test_split(trainX, trainY, test_size=0.2)
 
     algo = linear_model.LinearRegression()
     # algo = linear_model.LogisticRegression()
@@ -32,7 +30,8 @@ def getPred(timeCharged):
     list = []
     list.append(timeCharged)
     df = pd.DataFrame(list)
-    return '{0:.2f}'.format(algo.predict(df)[0]);
+    # '{0:.2f}'.format(algo.predict(df)[0]);
+    return "{0:.2f}".format(min(charge_time*2,8))
 
 
 if __name__ == '__main__':
