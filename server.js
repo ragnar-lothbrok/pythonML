b27b57e58784d1720b3090cccdb74292ccdc5618
RedViper <raghunitb@gmail.com>
2019-09-26 16:28:19 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+//Simple application
+const http = require('http');
+const url = require('url');
+
+var server = http.createServer((req, res) =>{
+    let pathname = url.parse(req.url).pathname;
+    console.log(pathname);
+    let body = '';
+    req.on('data', chunk => {
+        body += chunk.toString(); // convert Buffer to string
+    });
+    req.on('end', () => {
+        console.log(body);
+        res.end('ok');
+    });
+});
+
+server.listen(80, () => {
+    console.log('Server started on port 80');
+});
